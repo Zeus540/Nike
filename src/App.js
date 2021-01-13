d7f4550b950a48a66c631ba666a6794168320cbd
Zaheer <zaheerroberts4@gmail.com>
2021-01-13 20:36:41 +0200

    cahnged layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import Page2 from './components/page2.jsx'
 import Page3 from './components/page3.jsx'
 import Nav from './components/Nav.jsx'
 import Social from './components/Social.jsx'
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="App" >
         <Nav/>
-        <Page3/>
-      
+       
+        <Page2/>
         <Gallery1/>
         <Social/>
         <Form/>
